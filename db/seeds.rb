02c87e9c3f0f0d477ffbe7269934c546e65ef27b
Kent <kentslaymaker@gmail.com>
2025-02-07 14:38:24 -0700

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Satellite.destroy_all
+
+Satellite.create(
+  name: 'Earth'
+)
+Satellite.create(
+  name: 'Moon',
+  position_x: 332_899_299,
+  position_y: 192_199_500
+)
+Satellite.create(
+  name: 'Rocinante'
+)

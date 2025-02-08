83b240d49a185ee720439083ec43a80b1044787a
Kent <kentslaymaker@gmail.com>
2025-02-07 20:55:39 -0700

    current course migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_08_021943) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_08_035458) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -42,5 +42,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_08_021943) do
     t.integer "thrust", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "current_course_id"
   end
 end

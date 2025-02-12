c61e3422cbca503b8b3b04b7523d2ad770005df7
Kent <kentslaymaker@gmail.com>
2025-02-11 21:18:54 -0700

    formatting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @satellites = Satellite.order(:id)
-    @ship = Satellite.last
+    @ship       = Satellite.last
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,5 @@
 module HomeHelper
+  # TODO: put 0,0 at center screen, not bottom left
   def screen_position(pos, zoom)
     pos.to_f / zoom * 100
   end
